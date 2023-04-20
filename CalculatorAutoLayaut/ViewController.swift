3fad73ce57968f3ca89a00abc5035da6b628ee0e
Ali <128646611+Skaybun@users.noreply.github.com>
2023-04-20 16:33:56 +0300

    Initial Commit

---
+++ CalculatorAutoLayaut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorAutoLayaut
+//
+//  Created by Ali KINU on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
